880b59717dccaba1102fd3d88e2c1ecdd6cb5f46
Кандрушин Максим 22-IST-1 <125025831+heiby-baby@users.noreply.github.com>
2023-05-23 19:40:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,15 +6,11 @@
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  int index = n - 1;
-  std::vector<char> a;
-  std::vector<std::vector<char>> \
-    massivchik_nashix_perestanovok(tree.getPermutations());
-  if (massivchik_nashix_perestanovok.size() < n) {
-    return a;
+  std::vector<std::vector<char>> perm = tree.getPermutations();
+  if (n < 0 || perm.size() < n) {
+    return {};
   }
-  std::vector<char> \
-    nusjniu_nam_perestanovocha(massivchik_nashix_perestanovok[index]);
-  nusjniu_nam_perestanovocha.erase(nusjniu_nam_perestanovocha.begin());
-  return nusjniu_nam_perestanovocha;
+  std::vector<char> temp = perm[n - 1];
+  temp.erase(temp.begin());
+  return temp;
 }
